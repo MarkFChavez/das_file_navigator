3fa699a1abec3ee167a881c9b57d28482767c008
Mark <markchav3z@gmail.com>
2017-05-29 18:25:59 +0800

    better name

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
 
   Screencast = Struct.new(:name, :path) do
     def decorated_name
-      name.gsub("-", " ")[9, name.length].capitalize
+      name.gsub("-", " ")[9, name.length].split(".").first.capitalize
     end
   end
 
